8aa89967a68fcd3e8bafab8430b7691584ef2043
Huseyin Ozkan <ozkan.hsyn1@gmail.com>
2023-03-16 00:36:07 +0300

    Battle Location added

--- adventureGame/src/Game.java
+++ adventureGame/src/Game.java
@@ -17,9 +17,12 @@ public class Game {
             System.out.println();
             System.out.println("----------------Bölgeler----------------");
             System.out.println();
-            System.out.println("1 - Güvenli Ev");
-            System.out.println("2 - Mağaza");
-            System.out.println("0 - Çıkış Yap");
+            System.out.println("1 - Güvenli Ev --> burası sizin için güvenli bir ev, düşman yoktur !");
+            System.out.println("2 - Eşya Dükkanı --> Silah veya Zırh satın alabilirsiniz !");
+            System.out.println("3 - Mağara --> Ödül<Yemek>, dikkatli ol karşına zombi çıkabilir !");
+            System.out.println("4 - Orman --> Ödül<Odun>, dikkatli ol karşına vampir çıkabilir !");
+            System.out.println("5 - Nehir --> Ödül<Su>, dikkatli ol karşına ayı çıkabilir !");
+            System.out.println("0 - Çıkış Yap --> Oyunu sonlandır !");
             System.out.println("Lütfen gitmek istediğiniz bölgeyi seçiniz :");
         int selectloc = input.nextInt();
         switch (selectloc){
@@ -31,8 +34,17 @@ public class Game {
             case 2:
                 location = new ToolStore(player);
                 break;
+            case 3:
+                location = new Cave(player);
+                break;
+            case 4:
+                location = new Forest(player);
+                break;
+            case 5:
+                location = new River(player);
+                break;
             default:
-                location = new SafeHouse(player);
+                System.out.println("Lütfen geçerli bir bölge giriniz !");
         }
         if (location == null){
             System.out.println("Bu karanlık ve sisli adadan çabuk vazgeçtin !");
