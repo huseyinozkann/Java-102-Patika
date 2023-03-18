181927f5969b05424edd352a5c3663fae68439bd
Huseyin Ozkan <ozkan.hsyn1@gmail.com>
2023-03-18 13:22:31 +0300

    war algorithm written

--- adventureGame/src/Player.java
+++ adventureGame/src/Player.java
@@ -60,13 +60,15 @@ public class Player {
         System.out.println("Silahınız : " + this.getInventory().getWeapon().getName()+
                         ", Zırh : " + this.getInventory().getArmor().getName()+
                         ", Bloklama : " + this.getInventory().getArmor().getBlock()+
-                        ", Hasarınız : " + this.getDamage()+
+                        ", Hasarınız : " + this.getTotalDamage()+
                         ", Sağlık : "+ this.getHealth()+
                         ", Para : "+this.getMoney());
     }
-    public int getDamage() {
-
+    public int getTotalDamage(){
         return damage + this.getInventory().getWeapon().getDamage();
+    }
+    public int getDamage() {
+        return damage;
     }
 
     public void setDamage(int damage) {
